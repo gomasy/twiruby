79a6bf702ce68084077f3eeb15fa34fcd0b90cf8
Gomasy <nyan@gomasy.jp>
2014-12-25 05:01:10 +0900

    Update oauth_spec.rb: add examples

--- spec/twiruby/oauth_spec.rb
+++ spec/twiruby/oauth_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe TwiRuby::OAuth do
-  let(:instance) { TwiRuby::OAuth.new(consumer_key: "CK", consumer_secret: "CS", access_token: "AT", access_token_secret: "ATS") }
+  let(:instance) { TwiRuby::OAuth.new }
 
   describe "#get_request_token" do
     it "raise TwiRuby::Error::Unauthorized" do
@@ -16,6 +16,40 @@ describe TwiRuby::OAuth do
       expect do
         instance.get_access_token({"oauth_token" => "token", "oauth_token_secret" => "secret"})
       end.to raise_error TwiRuby::Error::Unauthorized
+    end
+  end
+
+  describe "#has_consumer_token?" do
+    subject { instance.has_consumer_token? }
+
+    it "should return false" do
+      instance.consumer_key = "CK"
+
+      is_expected.to be false
+    end
+
+    it "should return true" do
+      instance.consumer_key = "CK"
+      instance.consumer_secret = "CS"
+
+      is_expected.to be true
+    end
+  end
+
+  describe "#has_oauth_token?" do
+    subject { instance.has_oauth_token? }
+
+    it "should return false" do
+      instance.access_token = "AT"
+
+      is_expected.to be false
+    end
+
+    it "should return true" do
+      instance.access_token = "AT"
+      instance.access_token_secret = "ATS"
+
+      is_expected.to be true
     end
   end
 end
