b6cd0dcac63a71c98362b4c9bc077ccdd6a2eed5
Gomasy <nyan@gomasy.jp>
2015-01-04 13:31:09 +0900

    streaming/client.rb: add descriptions and fix codes

--- lib/twiruby/streaming/client.rb
+++ lib/twiruby/streaming/client.rb
@@ -10,26 +10,34 @@ module TwiRuby
         @user = Streaming::Request.new(tokens, Streaming::USER_BASE_URL)
       end
 
+      # Returns public statuses that match one or more filter predicates.
+      #
       def filter(options = {})
-        @public.post("/1.1/statuses/filter.json") do |chunk|
+        @public.post("/1.1/statuses/filter.json", nil, options) do |chunk|
           yield(chunk)
         end
       end
 
+      # Returns a small random sample of all public statuses.
+      #
       def sample(options = {})
-        @public.get("/1.1/statuses/sample.json") do |chunk|
+        @public.get("/1.1/statuses/sample.json", options) do |chunk|
           yield(chunk)
         end
       end
 
+      # Site Streams allows services, such as web sites or mobile push services, to receive real-time updates for a large number of users.
+      #
       def site(options = {})
-        @site.get("/1.1/site.json") do |chunk|
+        @site.get("/1.1/site.json", options) do |chunk|
           yield(chunk)
         end
       end
 
+      # User Streams provide a stream of data and events specific to the authenticated user.
+      #
       def user(options = {})
-        @user.get("/1.1/user.json") do |chunk|
+        @user.get("/1.1/user.json", options) do |chunk|
           yield(chunk)
         end
       end
