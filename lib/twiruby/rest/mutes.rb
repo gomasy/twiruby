9a93acc3cb6baae604caa319ff8689a780e4f9c8
Gomasy <nyan@gomasy.jp>
2016-08-23 10:04:46 +0900

    Add rest/mutes.rb

---
+++ lib/twiruby/rest/mutes.rb
@@ -0,0 +1,37 @@
+module TwiRuby
+  module REST
+    module Mutes
+      # Mutes the user specified in the ID parameter for the authenticating user.
+      #
+      # @see https://dev.twitter.com/rest/reference/post/mutes/users/create
+      # @param options [Hash]
+      def mutes_create(options = {})
+        @req.post("/1.1/mutes/users/create.json", nil, options)
+      end
+
+      # Un-mutes the user specified in the ID parameter for the authenticating user.
+      #
+      # @see https://dev.twitter.com/rest/reference/post/mutes/users/destroy
+      # @param options [Hash]
+      def mutes_destroy(options = {})
+        @req.post("/1.1/mutes/users/destroy.json", nil, options)
+      end
+
+      # Returns an array of numeric user ids the authenticating user has muted.
+      #
+      # @see https://dev.twitter.com/rest/reference/get/mutes/users/ids
+      # @param options [Hash]
+      def mutes_ids(options = {})
+        @req.get("/1.1/mutes/users/ids.json", options)
+      end
+
+      # Returns an array of user objects the authenticating user has muted.
+      #
+      # @see https://dev.twitter.com/rest/reference/get/mutes/users/list
+      # @param options [Hash]
+      def mutes_list(options = {})
+        @req.get("/1.1/mutes/users/list.json", options)
+      end
+    end
+  end
+end
